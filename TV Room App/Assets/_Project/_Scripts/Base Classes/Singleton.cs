274ae175dc86302194322eb8d70a9f93055b9616
lijuthomas1 <lijuthomas.job@gmail.com>
2024-05-16 06:45:14 +0530

    lock rearranged

--- TV Room App/Assets/_Project/_Scripts/Base Classes/Singleton.cs
+++ TV Room App/Assets/_Project/_Scripts/Base Classes/Singleton.cs
@@ -1,47 +1,55 @@
 using UnityEngine;
 namespace TV.Core
 {
-   /// <summary>
+    /// <summary>
     /// Base class of all singleton. Where T is Type Parameter, means class name.
     /// public class Myclass : Singleton<Myclass>{}
     /// </summary>
     public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
-       private static T _instance;
+        private static T _instance;
         private static readonly object _instanceLock = new object();
         private static bool _quitting = false;
-       public static T Instance
+        public static T Instance
         {
             get
             {
-                lock (_instanceLock)
+
+                if (_instance == null && !_quitting)
                 {
-                    if (_instance == null && !_quitting)
+                    lock (_instanceLock)
                     {
-                       _instance = GameObject.FindObjectOfType<T>();
+                        Debug.Log("here1");
+                        _instance = GameObject.FindObjectOfType<T>();
                         if (_instance == null)
                         {
                             GameObject go = new GameObject(typeof(T).ToString());
                             _instance = go.AddComponent<T>();
-                           DontDestroyOnLoad(_instance.gameObject);
+                            DontDestroyOnLoad(_instance.gameObject);
+                            Debug.Log("here2");
                         }
                     }
-                   return _instance;
+                   
                 }
+                return _instance;
             }
         }
-       protected virtual void Awake()
+        protected virtual void Awake()
         {
-            if (_instance == null) _instance = gameObject.GetComponent<T>();
+            if (_instance == null)
+            {
+                _instance = gameObject.GetComponent<T>();
+                Debug.Log("here3");
+            }
             else if (_instance.GetInstanceID() != GetInstanceID())
             {
                 Destroy(gameObject);
                 throw new System.Exception(string.Format("Instance of {0} already exists, removing {1}", GetType().FullName, ToString()));
             }
         }
-       protected virtual void OnApplicationQuit()
+        protected virtual void OnApplicationQuit()
         {
             _quitting = true;
         }
-   }
+    }
 }
